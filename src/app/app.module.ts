80605fd55ae8f8d12a6c1ba0250a2968df51bc07
leidypaez <leidy16paez@gmail.com>
2021-11-15 23:22:49 -0500

    se agrega el proyecto con diseños de login y ruteo de angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './component/header/header.component';
+import { FooterComponent } from './component/footer/footer.component';
+import { LoginComponent } from './component/login/login.component';
+import { RegisterComponent } from './component/register/register.component';
+import { LogoComponent } from './component/logo/logo.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    LoginComponent,
+    RegisterComponent,
+    LogoComponent
   ],
   imports: [
     BrowserModule,
